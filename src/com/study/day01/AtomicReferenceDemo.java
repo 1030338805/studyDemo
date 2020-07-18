ff81809740e0845a7e9b12db2b2406a97babff54
zhanglei <zhanglei@162.com>
2020-07-18 09:25:03 +0800

    ABA问题,Collection与线程安全，锁，BlockingQueue的使用

---
+++ src/com/study/day01/AtomicReferenceDemo.java
@@ -0,0 +1,27 @@
+package com.study.day01;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+* @Description:    原子引用
+* @Author:         zhangl
+* @CreateDate:     2020/7/17 7:55
+*/
+class User{
+    String name;
+    int age;
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+public class AtomicReferenceDemo {
+    public static void main(String[] args) {
+        User zhangsan = new User("zhangsan",5);
+        User lisi =  new User("lisi",10);
+        AtomicReference<User> atomicReference = new AtomicReference<User>();
+        atomicReference.set(zhangsan);
+        System.out.println(atomicReference.compareAndSet(zhangsan,lisi)+"\t"+atomicReference.get().name);
+        System.out.println(atomicReference.compareAndSet(zhangsan,lisi)+"\t"+atomicReference.get().name);
+    }
+}
