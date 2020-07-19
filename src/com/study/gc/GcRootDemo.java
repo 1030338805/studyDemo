9eab095baccdd6113c99962b45f2af68b9a35736
zhanglei <zhanglei@162.com>
2020-07-19 19:27:24 +0800

    GC

---
+++ src/com/study/gc/GcRootDemo.java
@@ -0,0 +1,13 @@
+package com.study.gc;
+/**
+* @Description:    GcRoot
+ * 哪些些可以作为GCRoot
+ * 1.虚拟机栈（局部变量）
+ * 2.方法中的类静态属性引用对象
+ * 3.方法区中常量的引用对象
+ * 4.本地方法栈中的Native放法
+* @Author:         zhangl
+* @CreateDate:     2020/7/19 8:12
+*/
+public class GcRootDemo {
+}
